b745af0fc29d4451d15a9a8788052c314da3fe15
Mark Carson <markcarson121@gmail.com>
2023-05-09 15:39:26 +0200

    created Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,1 +1,55 @@
 import type { FunctionComponent } from "react";
+import classnames from "classnames";
+
+type ExcludeFromTuple<T extends any[], U> = {
+  [K in keyof T]: T[K] extends U ? never : T[K];
+}[number];
+
+type Exclusive<T extends PropertyKey[], U = any> = T[number] extends infer E
+  ? E extends string
+    ? Record<E, U> & { [k in ExcludeFromTuple<T, E>]?: never }
+    : never
+  : never & {};
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  Partial<{ outline?: boolean; rounded?: boolean }> &
+  Exclusive<["primary", "secondary", "success", "warning", "danger"], boolean>;
+
+export const Button: FunctionComponent<ButtonProps> = ({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+  ...rest
+}) => {
+  const classes = classnames(
+    rest.className,
+    "flex items-center px-3 py-1.5 border",
+    {
+      "border-blue-500 bg-blue-500": primary,
+      "border-gray-900 bg-gray-900": secondary,
+      "border-green-500 bg-green-500": success,
+      "border-yellow-400 bg-yellow-400": warning,
+      "border-red-500 bg-red-500": danger,
+      "rounded-full": rounded,
+      "text-white":
+        !outline && (primary || secondary || success || warning || danger),
+      "bg-white": outline,
+      "text-blue-500": outline && primary,
+      "text-gray-500": outline && primary,
+      "text-green-500": outline && primary,
+      "text-yellow-500": outline && primary,
+      "text-red-500": outline && primary,
+    }
+  );
+
+  return (
+    <button {...rest} className={classes}>
+      {children}
+    </button>
+  );
+};

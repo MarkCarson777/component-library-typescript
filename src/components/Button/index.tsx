97e110e5d36d44c64725368a2011cf6311eced8d
Mark Carson <markcarson121@gmail.com>
2023-05-10 12:24:02 +0200

    add pseudo code for Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,10 +2,12 @@ import classnames from "classnames";
 
 import styles from "./index.module.css";
 
+// ExcludeFromTuple type returns a new tuple with all occurences of U removed from T
 type ExcludeFromTuple<T extends any[], U> = {
   [K in keyof T]: T[K] extends U ? never : T[K];
 }[number];
 
+// Create an object type where all props from tuple T are optional except one, which is specified by parameter U
 type Exclusive<T extends PropertyKey[], U = any> = T[number] extends infer E
   ? E extends string
     ? Record<E, U> & { [k in ExcludeFromTuple<T, E>]?: never }
@@ -54,7 +56,9 @@ export function Button(props: ButtonProps) {
       {...rest}
       className={classnames(
         className,
+        // default styling for all button variants
         "relative inline-flex justify-center items-center font-semibold text-white leading-none outline-none space-x-1",
+        // set colour according to button variant
         {
           "bg-blue-500": primary,
           "bg-gray-500": secondary,
@@ -62,15 +66,19 @@ export function Button(props: ButtonProps) {
           "bg-yellow-500": warning,
           "bg-red-500": danger,
         },
+        // set padding and border radius according to size prop
         size === "xs" && "px-2 rounded",
         (size === "s" || size === "m" || size === "l") && "px-5 rounded-lg",
         size === "xl" && "px-5 rounded-lg text-lg",
-        !disabled && primary && "hover:bg-blue-600",
-        !disabled && secondary && "hover:bg-gray-600",
-        !disabled && success && "hover:bg-green-600",
-        !disabled && warning && "hover:bg-yellow-600",
-        !disabled && danger && "hover:bg-red-600",
+        // styling for hover state of each variant
+        !disabled && !pending && primary && "hover:bg-blue-600",
+        !disabled && !pending && secondary && "hover:bg-gray-600",
+        !disabled && !pending && success && "hover:bg-green-600",
+        !disabled && !pending && warning && "hover:bg-yellow-600",
+        !disabled && !pending && danger && "hover:bg-red-600",
+        // styling for disabled and pending states
         (disabled || pending) && "opacity-50 pointer-events-none",
+        // if an SVG icon is in children then set margin according to size prop
         size === "xs" && styles.xs,
         size === "s" && styles.sm,
         size === "m" && styles.med,
@@ -78,11 +86,13 @@ export function Button(props: ButtonProps) {
         size === "xl" && styles.xlg,
         size === "auto" && styles.auto
       )}
+      // adjust height of button according to size prop
       style={{
         height: sizes[size],
         ...style,
       }}
     >
+      {/* adjust font size and spacing according to size prop */}
       <div
         className={classnames(
           "flex items-center",
@@ -91,6 +101,7 @@ export function Button(props: ButtonProps) {
           (size === "m" || size === "l" || size === "xl") && "space-x-3"
         )}
       >
+        {/* if pending display loader component */}
         {pending && <div className="text-[0.5rem]">Insert Loader</div>}
         {children}
       </div>

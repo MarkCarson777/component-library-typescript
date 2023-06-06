d986e5628e67cceff8944d2562995346cb4a6136
Mark Carson <markcarson121@gmail.com>
2023-06-06 12:01:02 +0200

    added basic Avatar component

--- src/components/Avatar/index.stories.tsx
+++ src/components/Avatar/index.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Avatar } from "./";
+import Avatar from "./";
 
 const meta: Meta<typeof Avatar> = {
   title: "components/Avatar",
@@ -14,5 +14,10 @@ export const Default: Story = {
   // remove no control warning from Storybook
   parameters: {
     controls: { hideNoControlsWarning: true },
+  },
+  args: {
+    name: "Mark",
+    size: 40,
+    imageUrl: "nope",
   },
 };
--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,3 +1,26 @@
-export function Avatar() {
-  return <div>Avatar</div>;
-}
+type AvatarProps = {
+  name: string;
+  imageUrl: string;
+  size?: number;
+};
+
+const Avatar: React.FC<AvatarProps> = ({ name, imageUrl, size = 40 }) => {
+  return (
+    <div
+      style={{
+        width: size,
+        height: size,
+        borderRadius: "50%",
+        overflow: "hidden",
+      }}
+    >
+      <img
+        src={imageUrl}
+        alt={name}
+        style={{ width: "100%", height: "100%", objectFit: "cover" }}
+      />
+    </div>
+  );
+};
+
+export default Avatar;

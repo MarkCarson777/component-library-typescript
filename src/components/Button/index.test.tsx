cdf0c9d4d435e3d65e7efbdfc689ab5afb3f2d19
Mark Carson <markcarson121@gmail.com>
2023-07-03 09:50:29 +0200

    fixed spacing and added future tests for Button component

--- src/components/Button/index.test.tsx
+++ src/components/Button/index.test.tsx
@@ -32,3 +32,23 @@ describe("display the correct label", async () => {
     expect(button.textContent).toBe("Click Me!");
   });
 });
+
+// Render the button component with different combinations of props and ensure it renders without errors.
+
+// Verify that the button text or child elements are rendered correctly.
+
+// Simulate a click event on the button and assert that the onClick prop is called.
+
+// Verify that the appropriate CSS classes are applied based on the props passed to the component.
+
+// Test different button variants (primary, secondary, success, warning, danger) and assert that the correct background color is applied.
+
+// Test different button sizes (xs, s, m, l, xl, auto) and assert that the button has the correct padding, border radius, and font size.
+
+// Test the hover state of the button and assert that the appropriate CSS classes are applied.
+
+// Test the button's disabled state by setting the disabled prop to true and assert that the button is non-interactive and has reduced opacity.
+
+// Test the pending state by setting the pending prop to true and assert that the loader component is displayed.
+
+// Test the integration with the Loader component by asserting that it is rendered when the pending prop is true.

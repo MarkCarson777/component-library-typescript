8d4984e99c94dbdffe1deb1b7dc5888e18701fec
Mark Carson <markcarson121@gmail.com>
2023-07-04 15:24:54 +0200

    created basic footer comp[onent

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+export const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2023 Your Website</p>
+      </div>
+    </footer>
+  );
+};

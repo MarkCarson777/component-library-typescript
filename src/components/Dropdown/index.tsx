14c23c01e65ba3ac9c8b781d7b0dd248985ea30d
Mark Carson <markcarson121@gmail.com>
2023-07-04 15:27:38 +0200

    created basic dropdown component

---
+++ src/components/Dropdown/index.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+type DropdownOption = {
+  value: string;
+  label: string;
+};
+
+type DropdownProps = {
+  options: DropdownOption[];
+  onSelect: (selectedValue: string) => void;
+};
+
+export const Dropdown: React.FC<DropdownProps> = ({ options, onSelect }) => {
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  const handleOptionSelect = (option: DropdownOption) => {
+    setSelectedOption(option.value);
+    onSelect(option.value);
+  };
+
+  return (
+    <select
+      value={selectedOption || ""}
+      onChange={(e) => handleOptionSelect(options[e.target.selectedIndex])}
+    >
+      <option value="" disabled>
+        Select an option
+      </option>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+};

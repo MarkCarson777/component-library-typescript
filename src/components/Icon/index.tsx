4c401168ab210381ea2a2126ff6348a08eb6541a
Mark Carson <markcarson121@gmail.com>
2023-07-11 11:41:42 +0200

    built Icon component

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,3 +1,31 @@
-export function Icon() {
-  return <div>Icon</div>;
+import { ReactElement } from "react";
+import { ReactComponent as Search } from "./icons/search.svg";
+import { ReactComponent as Star } from "./icons/star.svg";
+
+interface IconProps {
+  icon: string;
+  color?: string;
+  [key: string]: any;
+}
+
+interface IconComponents {
+  [key: string]: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+}
+
+const icons: IconComponents = {
+  Search,
+  Star,
+};
+
+export function Icon({ icon, color, ...rest }: IconProps): ReactElement {
+  const Component = icons[icon];
+  const styles = {
+    fill: color,
+  };
+
+  return (
+    <div style={styles}>
+      <Component {...rest} />
+    </div>
+  );
 }
